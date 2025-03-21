94cd620ae03665a315ebdcf70cd6502028bc7886
XWcode11 <1039954680@qq.com>
2025-03-21 16:57:27 +0800

    重构run_pipeline.sh以动态生成输出目录和文件名，简化结果存储结构

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -2,12 +2,29 @@
 antigen_hlt_pdb=$1
 antibody_hlt_pdb=$2
 
+# get filenames and remove the extension
+antigen_filename=$(basename "$antigen_hlt_pdb")
+antigen_filename="${antigen_filename%.*}"
+
+antibody_filename=$(basename "$antibody_hlt_pdb")
+antibody_filename="${antibody_filename%.*}"
+
+output_name=${antigen_filename}_${antibody_filename}
+
 
 # create a directory to store the rfdiffusion results
-rfdiffusion_output_dir=/home/output_data/rfdiffusion/${antibody_hlt_pdb}_${antigen_hlt_pdb}
+rfdiffusion_output_dir=/home/output_data/rfdiffusion/$output_name
 mkdir -p $rfdiffusion_output_dir
 
-output_prefix=$rfdiffusion_output_dir/${antibody_hlt_pdb}_${antigen_hlt_pdb}
+# create a directory to store the proteinmpnn results
+proteinmpnn_output_dir=/home/output_data/proteinmpnn/$output_name
+mkdir -p $proteinmpnn_output_dir
+
+# create a directory to store the rf2 results
+rf2_output_dir=/home/output_data/rf2/$output_name
+mkdir -p $rf2_output_dir
+
+output_prefix=$rfdiffusion_output_dir/$output_name
 
 poetry run python  /home/src/rfantibody/rfdiffusion/rfdiffusion_inference.py \
     --config-name antibody \
@@ -19,21 +36,19 @@ poetry run python  /home/src/rfantibody/rfdiffusion/rfdiffusion_inference.py \
     inference.num_designs=20 \
     inference.output_prefix=$output_prefix
 
-# create a directory to store the proteinmpnn results
-proteinmpnn_output_dir=/home/output_data/proteinmpnn/${antibody_hlt_pdb}_${antigen_hlt_pdb}
-mkdir -p $proteinmpnn_output_dir
+
 
 poetry run python /home/scripts/proteinmpnn_interface_design.py \
     -pdbdir $rfdiffusion_output_dir \
     -outpdbdir $proteinmpnn_output_dir
 
-# create a directory to store the rf2 results
-rf2_output_dir=/home/output_data/rf2/${antibody_hlt_pdb}_${antigen_hlt_pdb}
-mkdir -p $rf2_output_dir
+
 
 poetry run python /home/scripts/rf2_predict.py \
     input.pdb_dir=$proteinmpnn_output_dir \
     output.pdb_dir=$rf2_output_dir 
 
 # extract the scores from the rf2 results
-poetry run python extract_pdb_scores.py $rf2_output_dir --output $rf2_output_dir/${antibody_hlt_pdb}_${antigen_hlt_pdb}_scores.csv 
+poetry run python extract_pdb_scores.py $rf2_output_dir --output $rf2_output_dir/${output_name}_scores.csv 
+
+echo $output_name "finished!"
